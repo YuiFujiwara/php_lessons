30ac35bf464bd1cd78986cf60ed03354b1344bf2
YuiFujiwara <48627138+YuiFujiwara@users.noreply.github.com>
2019-04-17 21:28:06 +0900

    Create index.php
    #2 はじめてのPHPプログラム

---
+++ index.php
@@ -0,0 +1,20 @@
+<?PHP
+
+//コメント
+# コメント
+
+/*
+コメント
+コメント
+コメント
+*/
+
+echo "hello from the TOP!";
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+  <p>Hello World <?php echo " from PHP"; ?></p>
+</body>
+</html>

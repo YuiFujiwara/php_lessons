35bcd5f3cfb7ae33ba515bd81c30b1c6d9690eab
YuiFujiwara <48627138+YuiFujiwara@users.noreply.github.com>
2019-04-17 23:02:03 +0900

    Update index.php
    #5 定数を使う
    var_dump(__LINE__); , var_dump(__FILE__); , var_dump(__DIR__);は、
    自動的に定義される定数の代表例。

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?PHP
 
-/*
-変数: データにつけるラベル
+// 定数: 変更されない値につけるラベル（全て大文字を使う）
 
-データ型:
-- 文字列型 string
-- 数値 integer float
-- 論理 boolean / true false
-- 配列
-- オブジェクト
-- null
-*/
+define("MY_EMAIL", "taguchi@dotinstall.com");
 
-$msg = "hello from the TOP!";
-echo $msg;
-var_dump($msg);
+echo MY_EMAIL;
+// MY_EMAIL = "hogehoge";
+
+var_dump(__LINE__); //10
+var_dump(__FILE__);
+var_dump(__DIR__);

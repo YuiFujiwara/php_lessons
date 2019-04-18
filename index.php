38b80f8ec3204cd7a0da590659df3258df3edd83
YuiFujiwara <48627138+YuiFujiwara@users.noreply.github.com>
2019-04-18 19:58:19 +0900

    Update index.php
    #26 名前空間を使う

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?PHP
 
-// require : エラー出たとき、fatal error (が発生してその場で処理終了)
-// require_once (onceがつくとphpが自動的にそのファイルが読み込まれてるかを自動的にチェックして読み込まれてたらそれをスキップしてくれる)
+// 名前空間
 
-// require "User.class.php";
+require "User.class.php";
 
-// include : warning (を発生させて処理を続行)
-// include_once
+// use Dotinstall\Lib as Lib;
+use Dotinstall\Lib;
 
-// autoload : クラスにしか使えない(クラスが未定義だった場合に自動的に実行される)
-
-spl_autoload_register(function($class) {
-	require $class . ".class.php";
-});
-
-$bob = new User("Bob");
+$bob = new Lib\User("Bob");
 $bob->sayHi();

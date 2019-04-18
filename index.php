ce0eaef29612efa5b819700d655aaf76bcc684ae
YuiFujiwara <48627138+YuiFujiwara@users.noreply.github.com>
2019-04-18 11:41:39 +0900

    Update index.php
    #10 switch文で条件分岐

--- index.php
+++ index.php
@@ -1,27 +1,21 @@
 <?PHP
 
-// 真偽値
-/*
-# falseになる場合に使える
-文字列 : 空、"0"
-数値 : 0、0.0
-論理値 : false
-配列 : 要素の数が0
-null
-*/
+// switch 条件分岐
 
-$x = 5;
-if ($x) {
-  // if ($x == true) {
-  echo "great!";
-}
+$signal = "green";
 
-// 三項演算子
-
-$max = ($a > $b) ? $a : $b; // $a > $bが真なら$aを偽なら$bをmaxに代入しなさい
-
-if ($a > $b) {
-  $max = $a;
-} else {
-  $max = $b;
+switch ($signal) {
+  case "red" :
+    echo "stop!";
+    break;
+  case "blue" :
+  case "green": //条件増やしたいときは、caseをそのまま追加すれば良い
+    echo "go!";
+    break;
+  case "yello" :
+    echo "caution!";
+    break;
+  default:
+    echo "wrong signal";
+    break;
 }

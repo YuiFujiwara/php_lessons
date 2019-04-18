d99305dcae40e299fde6f67a978677fef5f80291
YuiFujiwara <48627138+YuiFujiwara@users.noreply.github.com>
2019-04-18 13:18:34 +0900

    Update index.php
    #19 クラスを作る

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?PHP
 
-// 関数
+// User
 
-$x  = 5.6;
-// echo ceil($x); //6 小数点以下を繰り上げる
-// echo floor($x); //5 小数点以下を切り捨てる
-// echo round($x); //6 四捨五入
-// echo rand(1, 10); //1〜10の数字をランダムに引っ張る
+class User {
+  // property （変数）
+  public $name;
 
-$s1 = "hello";
-$s2 = "ねこ";
-// echo strlen($s1); //5 $1の文字数を数える
-// echo mb_strlen($s2); //2 $2の文字数を取得
-// printf("%s - %s -%.3f", $s1, $s2, $x); // %.3fは小数点3位まで表示させる
+  // constructor （定数）
+  public function __construct($name) {
+    $this->name = $name;
+  }
 
-$colors = ["red", "blue", "pink"];
-echo count($colors);
-echo implode("@", $colors); // implode:配列の要素をある区切り文字を挟んで連結（この場合は@）
+  // method （定数）
+  public function sayHi() {
+    echo "hi, i am $this->name!";
+  }
+}
+
+$tom = new User("Tom");
+$bob = new User("Bob");
+
+echo $tom->name; //Tom
+$bob->sayHi(); // hi, i am Bob!

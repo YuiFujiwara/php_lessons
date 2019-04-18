905b1ae3bc6f3d2e8c11d998c19e7f0f143d6404
YuiFujiwara <48627138+YuiFujiwara@users.noreply.github.com>
2019-04-18 19:07:25 +0900

    Update index.php
    #23 抽象クラス

--- index.php
+++ index.php
@@ -1,29 +1,14 @@
 <?PHP
 
-// アクセス権
-// - private: そのクラス内からのみアクセス可能
-// - protected: そのクラス+親子クラス内からのみアクセス可能
-// - public: どこからでもアクセス可能
+// 抽象クラス
 
-class User {
-  // public $name;
-  // private $name;
-  protected $name;
-  public function __construct($name) {
-    $this->name = $name;
-  }
+abstract class BaseUser {
+  public $name;
+  abstract public function sayHi();
+}
+
+class User extends BaseUser {
   public function sayHi() {
-    echo "hi, i am $this->name!";
+    echo "hello from User";
   }
 }
-
-class AdminUser extends User {
-  public function sayHello() {
-  echo "hello from $this->name";
-  }
-}
-
-$tom = new User("Tom");
-// echo $tom->name;
-$steve = new AdminUser("Steve");
-$steve->sayHello();
